61a663bd36f69782bf1ac7c2479d692ed52a0bf9
Bradley Kang <bkang@lcs.on.ca>
2021-10-06 22:42:27 -0400

    Initial Commit

---
+++ SwiftUI Starter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Starter
+//
+//  Created by Brad Kang on 2021-10-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI Starter/SwiftUI_StarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_StarterApp.swift
+//  SwiftUI Starter
+//
+//  Created by Brad Kang on 2021-10-06.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_StarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
